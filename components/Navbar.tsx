b1664d8d8d2e798b7126083feab9b81734c3c07f
akibbhuiyan <akibbhh@gmail.com>
2023-10-13 15:39:17 +0600

    bug fixed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,6 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
 
 const navIcons = [
   { src: "/assets/icons/search.svg", alt: "search" },
